4e712775f223057679badebe90e7a88d766106dc
MisterPy15 <uzumakirene006@gmail.com>
2024-06-22 10:57:36 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+
+if (bar) {
+    bar.addEventListener('click', ()=> {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', (event) => {
+        event.preventDefault(); // Empêche le rafraîchissement de la page
+        nav.classList.remove('active');
+    })
+}
+
+
+
+
+
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+            
+        }
+smallimg[1].onclick = function(){
+     MainImg.src = smallimg[1].src;
+
+        }
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+
+        }
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
+
+        }
